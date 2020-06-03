596c0062d9ef8b5d27364709282c035c21631bef
nikulkafedor <veronikafed@mail.ru>
2020-06-03 15:17:50 +0400

    lll

--- src/main/java/ru/spsuace/homework6/account/AccountManager.java
+++ src/main/java/ru/spsuace/homework6/account/AccountManager.java
@@ -12,9 +12,23 @@ public class AccountManager {
      * Метод переводит деньги с одного счета, на другой.
      * Если на исходном счету нет нужного количества денег, то метод возвращает false.
      * Класс Account непотокобезопасный и менять его нельзя
-     *
      */
     public boolean transfer(Account from, Account to, long sum) {
-        return false;
+        Object lock1 = from.hashCode() < to.hashCode() ? from : to;
+        Object lock2 = from.hashCode() < to.hashCode() ? to : from;
+        synchronized (lock1) {
+            synchronized (lock2) {
+                if (from.getBalance() < sum) {
+                    return false;
+                } else {
+                    from.withdraw(sum);
+                    to.deposit(sum);
+                    return true;
+                }
+            }
+        }
     }
 }
+
+
+
